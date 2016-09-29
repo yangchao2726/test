ccb305378f64b422761e8ff076f94ee01b6409e2
yangchao2726 <yangchao2726@163.com>
2016-09-29 17:47:50 +0800

    Person.java
    bean对象

---
+++ src/com/yc/test/Person.java
@@ -0,0 +1,24 @@
+/**
+ * @Description:TODO
+ * @author:YC
+ * @time:2016年9月29日 下午5:41:39
+ */
+package com.yc.test.bean;
+
+/**
+ * @Description:TODO
+ * @author:YC
+ * @time:2016年9月29日 下午5:41:39
+ */
+public class Person {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
