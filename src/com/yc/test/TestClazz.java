faf8824a5f61ab138ad06400a35ec53c53cb7fe2
yangchao2726 <yangchao2726@163.com>
2016-09-29 17:11:33 +0800

    第一版：建立项目

---
+++ src/com/yc/test/TestClazz.java
@@ -0,0 +1,28 @@
+/**
+ * @Description:TODO
+ * @author:YC
+ * @time:2016年9月29日 下午5:07:21
+ */
+package com.yc.test;
+
+/**
+ * @Description:TODO
+ * @author:YC
+ * @time:2016年9月29日 下午5:07:21
+ */
+public class TestClazz {
+
+	/**
+	 * @Description:
+	 * @param args
+	 * return:void
+	 * @exception:
+	 * @author: YC
+	 * @time:2016年9月29日 下午5:07:21
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test message:Hello World!");
+		System.out.println("TestClazz.main()");
+	}
+
+}
